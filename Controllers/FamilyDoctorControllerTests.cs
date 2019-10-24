99a78c17f69676ed4bc0648519ac3a9f36d5e4ed
Hansz,Jeremey J.J. van der <j.vanderhansz@student.fontys.nl>
2019-10-24 09:51:38 +0200

    Initial commit

---
+++ Controllers/FamilyDoctorControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiaLink.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace DiaLink.Controllers.Tests
+{
+    [TestClass()]
+    public class FamilyDoctorControllerTests
+    {
+        [TestMethod()]
+        public void FamilyDoctorTest_GetPatientData_Success()
+        {
+            //Arrange
+            FamilyDoctorController family = new FamilyDoctorController();
+            //Act
+            IActionResult result = family.FamilyDoctor();
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
